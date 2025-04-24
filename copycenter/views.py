ae0a1c1aaf388eee281a91a998795e7a6bdcd061
User <hplotter854@gmail.com>
2025-04-24 20:24:12 +0300

    OrderHistory

--- copycenter/views.py
+++ copycenter/views.py
@@ -123,6 +123,10 @@ class OrderViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
+    def get_queryset(self):
+        user = self.request.user
+        return Order.objects.filter(user=user)
+
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
